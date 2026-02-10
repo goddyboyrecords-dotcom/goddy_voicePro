6c5c68e092c3d427a0a8e46ff9584055400bc9cf
Goddy boy <goddyboyrecords@gmail.com>
2026-02-10 03:41:34 +0100

    Create siteConfig.js file with site configuration

---
+++ src/data/siteConfig.js
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Goddy Boy",
+  tagline: "Voice of purpose. Sound of truth.",
+  description:
+    "Goddy Boy is an upcoming artist sharing stories of struggle, hope, and resilience through sound.",
+  socials: {
+    spotify: "https://spotify.com/",
+    audiomack: "https://audiomack.com/",
+    soundcloud: "https://soundcloud.com/"
+  },
+  contactEmail: "goddyboy@gmail.com"
+};
